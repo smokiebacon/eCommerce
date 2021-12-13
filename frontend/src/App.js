72c71001ec8a56532ac8839934f5381923a074e0
Abraham Hsu <smokiebacon@gmail.com>
2021-12-13 14:40:24 -0800

    implement React Router v6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Container } from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
 
 const App = () => {
   return (
-    <>
-    <Header />
-    <main className="py-3">
-      <Container>
-        <h1>Welcome Home!</h1>
-     </Container>
-     </main>
-     <Footer />
-    </>
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+            <Routes>
+              <Route path='/' element={<HomeScreen/>} />
+              <Route path='/product/:id' element={<ProductScreen/>} />
+            </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   );
 }
 
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,14 @@
 import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 const Header = () => {
     return (
 <header>
     <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
     <Container fluid>
-        <Navbar.Brand href="/">Boba Origin</Navbar.Brand>
+        <LinkContainer to="/">
+            <Navbar.Brand>Boba Origin</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
         <Nav
@@ -13,8 +16,12 @@ const Header = () => {
             style={{ maxHeight: '100px' }}
             navbarScroll
         >
-            <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-            <Nav.Link href="/login"><i className='fas fa-user'></i>Sign In</Nav.Link>
+            <LinkContainer to="/cart">
+                <Nav.Link><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
+            </LinkContainer>
+            <LinkContainer to="/login">
+                <Nav.Link><i className='fas fa-user'></i>Sign In</Nav.Link>
+            </LinkContainer>
         </Nav>
         </Navbar.Collapse>
     </Container>
