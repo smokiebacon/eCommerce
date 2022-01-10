320e8122d4c2d86fcf193fa35b19c75b0e7c1bed
Abraham Hsu <smokiebacon@gmail.com>
2022-01-10 12:59:22 -0800

    adds mongodb and models

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,12 +1,21 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Card, Button, ListGroupItem } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap'
 import Rating from '../components/Rating'
-import products from '../products'
+import axios from 'axios';
 
 const ProductScreen = () => {
-    const params = useParams()
-    const product = products.find(p => p._id === params.id)
+    const params = useParams();
+    const [product, setProduct] = useState([])
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${params.id}`)
+            setProduct(data)
+        }
+        fetchProduct()
+    }, [params])
+   
     return (
         <div>
             <Link className='btn btn-dark my-3' to='/'> Go Back
